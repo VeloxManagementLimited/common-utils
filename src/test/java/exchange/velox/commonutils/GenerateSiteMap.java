d193d44030b921deee6b73b5e2adbe6222635589
dbui-velotrade <dbui@velotrade.com>
2020-02-10 13:38:19 +0700

    update sitemap tool

--- src/test/java/exchange/velox/commonutils/GenerateSiteMap.java
+++ src/test/java/exchange/velox/commonutils/GenerateSiteMap.java
@@ -1,5 +1,7 @@
 package exchange.velox.commonutils;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -14,28 +16,42 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GenerateSiteMap {
 
+
+    private List<String> readPatterns() throws IOException {
+        InputStream inclusions = getClass().getClassLoader().getResourceAsStream("sitemap-inclusions.txt");
+        List<String> lines = IOUtils.readLines(inclusions, "UTF-8");
+        List<String> results = new ArrayList<>(lines.size());
+        for (String line : lines) {
+            line = StringUtils.replace(line, ".", "\\.");
+            line = StringUtils.replace(line, "/", "\\/");
+            results.add(line);
+        }
+        return results;
+    }
+
     @Test
     public void generateSiteMap() throws Exception {
-        URL url = getClass().getClassLoader().getResource("example.xml");
+        URL url = getClass().getClassLoader().getResource("input.xml");
         File fXmlFile = new File(url.toURI());
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(fXmlFile);
         doc.getDocumentElement().normalize();
         NodeList nList = doc.getElementsByTagName("url");
-        List<Pattern> patterns = new ArrayList<>();
-        patterns.add(Pattern.compile("\\/global\\/"));
-        patterns.add(Pattern.compile("\\/vn\\/"));
+        List<String> lines = readPatterns();
+        List<Pattern> patterns = new ArrayList<>(lines.size());
+        for (String line : lines) {
+            patterns.add(Pattern.compile(line));
+        }
 
         Set<Element> removeNodes = new HashSet<>();
         for (int i = 0; i < nList.getLength(); i++) {
@@ -65,8 +81,10 @@ public class GenerateSiteMap {
         transformer.setOutputProperty( "{http://xml.apache.org/xslt}indent-amount", "2" );
         DOMSource source = new DOMSource(doc);
 
-        StreamResult result = new StreamResult(new File("src/test/resources/result.xml"));
+        String resultFilePath = "src/test/resources/copyscape-sitemap.xml";
+        StreamResult result = new StreamResult(new File(resultFilePath));
         transformer.transform(source, result);
+        System.out.println("OUTPUT: " + resultFilePath);
     }
 
     private boolean exclude(String url, List<Pattern> patterns) {
